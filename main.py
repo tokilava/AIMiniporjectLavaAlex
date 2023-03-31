48f3119d413861f4c61a310864415ca1f374225c
Alexander L <sala20@student.aau.dk>
2023-03-31 12:10:40 +0200

    Categorized weather conditions and checked for duplicates

--- main.py
+++ main.py
@@ -15,3 +15,22 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+#Load the CSV file to read the weather data
+weatherData = pd.read_csv('seattle-weather.csv')
+
+#Categorize weather conditions with numbers
+"""
+ Drizzle = 0
+    Rain = 1
+     Sun = 2
+    Snow = 3
+     Fog = 4
+"""
+
+weatherData = weatherData.replace({'drizzle': '0', 'rain': '1', 'sun': '2', 'snow': '3', 'fog': '4'})
+print(weatherData)
+
+#Check and drop if the weather data has duplicate entries
+#weatherData.drop_duplicates()
+#print(weatherData)
