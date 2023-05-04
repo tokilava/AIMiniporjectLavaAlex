62b5a8f8d5a99533ce21b82b3f1bbc13143e1318
Alexander L <sala20@student.aau.dk>
2023-05-04 15:01:40 +0200

    Removed unused print.

--- main.py
+++ main.py
@@ -9,20 +9,6 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
 #Load the CSV file to read the weather data
 weatherData = pd.read_csv('seattle-weather.csv')
 
